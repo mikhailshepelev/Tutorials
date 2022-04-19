357dbf6924a458f5276f2360229ebff739092a17
m.shepelev <m.shepelev@sportradar.com>
2022-04-19 16:54:55 +0300

    minor tweak

--- React/sending-http-requests-demo/src/App.js
+++ React/sending-http-requests-demo/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch('https://react-http-6b4a6.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
