c6a2ceb54e05c197270849ad5e65ad8c9efab2f8
mikhailshepelev <romanist-mike@yandex.ru>
2022-08-09 16:28:12 +0300

    Java course - completed 148 parts

---
+++ Java/src/section_11_namings_packages_static_final_keywords/scope_challenge/Main.java
@@ -0,0 +1,10 @@
+package section_11_namings_packages_static_final_keywords.scope_challenge;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        X x = new X(new Scanner(System.in));
+        x.x();
+    }
+}
